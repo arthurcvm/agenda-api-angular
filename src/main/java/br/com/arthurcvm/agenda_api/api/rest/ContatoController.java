0ec734ee4dfc12d6667dc35d0db63953dbdf9c2d
Arthur Cezar <arthurcvm@hotmail.com>
2024-08-25 17:02:58 -0300

    implementação de upload de foto

--- src/main/java/br/com/arthurcvm/agenda_api/api/rest/ContatoController.java
+++ src/main/java/br/com/arthurcvm/agenda_api/api/rest/ContatoController.java
@@ -2,10 +2,14 @@ package br.com.arthurcvm.agenda_api.api.rest;
 
 import br.com.arthurcvm.agenda_api.model.entity.Contato;
 import br.com.arthurcvm.agenda_api.model.repository.ContatoRepository;
+import jakarta.servlet.http.Part;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,5 +45,23 @@ public class ContatoController {
             c.setFavorito(!favorito);
             repository.save(c);
         });
+    }
+
+    @PutMapping("{id}/foto")
+    public byte[] addPhoto(@PathVariable Integer id, @RequestParam("foto") Part arquivo){
+        Optional<Contato> contato = repository.findById(id);
+        return contato.map( c -> {
+           try {
+               InputStream is = arquivo.getInputStream();
+               byte[] bytes = new byte[(int) arquivo.getSize()];
+               IOUtils.readFully(is, bytes);
+               c.setFoto(bytes);
+               repository.save(c);
+               is.close();
+               return bytes;
+           } catch (IOException e){
+                return null;
+           }
+        }).orElse(null);
     }
 }
